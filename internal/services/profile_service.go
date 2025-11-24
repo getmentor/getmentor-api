0631f531265a7d63f49fefc1aa15240fe84c8622
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-11-24 23:35:09 +0100

    use the right field for tags while profile edit

--- internal/services/profile_service.go
+++ internal/services/profile_service.go
@@ -73,7 +73,7 @@ func (s *ProfileService) SaveProfile(id int, token string, req *models.SaveProfi
 		"Workplace":    req.Workplace,
 		"Experience":   req.Experience,
 		"Price":        req.Price,
-		"Tag Links":    tagIDs,
+		"Tags Links":   tagIDs,
 		"Details":      req.Description,
 		"About":        req.About,
 		"Competencies": req.Competencies,
