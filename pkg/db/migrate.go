36aa497b81fff3c016fd516aabe6bc02adf7d129
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2026-02-12 12:34:14 +0100

    configure tls for migrations

--- pkg/db/migrate.go
+++ pkg/db/migrate.go
@@ -1,13 +1,14 @@
 package db
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file" // Register file source driver
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
 // RunMigrations executes database migrations from the specified path
@@ -17,11 +18,23 @@ import (
 //
 // Returns error if migrations fail, ignores ErrNoChange (already up to date)
 func RunMigrations(databaseURL, migrationsPath string) error {
-	// Open database connection
-	db, err := sql.Open("postgres", databaseURL)
+	// Parse connection config from URL
+	connConfig, err := pgx.ParseConfig(databaseURL)
 	if err != nil {
-		return fmt.Errorf("failed to open database: %w", err)
+		return fmt.Errorf("failed to parse database URL: %w", err)
 	}
+
+	// Configure TLS using the same CA cert as the main connection pool
+	tlsConfig, err := configureTLS(databaseURL)
+	if err != nil {
+		return fmt.Errorf("failed to configure TLS: %w", err)
+	}
+	if tlsConfig != nil {
+		connConfig.TLSConfig = tlsConfig
+	}
+
+	// Open database connection via pgx stdlib adapter
+	db := stdlib.OpenDB(*connConfig)
 	defer db.Close()
 
 	// Ping database to verify connection
